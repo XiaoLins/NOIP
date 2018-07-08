787454969a66d4abca8f2a3750abd5ebbb1cee13
XiaoLins <root@earthsky.cn>
2018-07-08 23:17:58 +0800

    Turing Day 2

---
+++ Turing/Day2/c.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <utility>
+#include <stack>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+long long int n,k;
+
+int main(){
+    scanf("%d %d",n,k);
+    
+    return 0;
+}
